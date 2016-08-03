a4d88f653fc3868898a078a13b9b254f9e17fb4a
Fin Christensen <christensen.fin@gmail.com>
2016-08-03 18:05:15 +0200

    Fix some issues with autocompletion

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #! /bin/bash -e
 
-git fetch && git merge --ff-only && git submodule update --init --rebase --recursive --remote
+echo "Updating git repository"
+
+(
+    git fetch && git merge --ff-only && git submodule update --init --rebase --recursive --remote
+) > /dev/null 2>&1 || echo "Failed to update git repository"
+
+echo "Installing typescript-tools locally"
 
 (
     cd typescript-tools
     npm install --prefix .
     make
-)
+) > /dev/null 2>&1 || echo "Failed to install typescript tools"
 
 BOOTSTRAP=";;;;;;;;;;;;;;;;;;;
 ;; SETUP FINMACS ;;
@@ -14,7 +20,21 @@ BOOTSTRAP=";;;;;;;;;;;;;;;;;;;
 (load-file \"~/.emacs.d/lisp/fin-macs.el\")
 "
 
-if ! grep -i "finmacs" ~/.emacs
+if ! grep -i "finmacs" ~/.emacs > /dev/null 2>&1
 then
+    echo "Bootstrapping emacs"
     echo "${BOOTSTRAP}" >> ~/.emacs
 fi
+
+echo "Installing FinMacs desktop file"
+cp finmacs.desktop ~/.local/share/applications/
+
+echo
+echo "You may add the following line to your local shell rc file:"
+echo "    alias emacs='emacs -fh -g 153x74'"
+echo
+
+echo "For typescript-tools to work properly please write the following lines to /usr/bin/tss"
+echo
+echo "#! /bin/bash"
+echo '/home/<your-user>/.emacs.d/typescript-tools/bin/tss --module amd $@'
